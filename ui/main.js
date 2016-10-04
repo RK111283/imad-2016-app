afb524e04edbd9e2e7a831325fb68b3d236085f0
RK111283 <r.kachhwaha@jnvu.edu.in>
2016-10-04 08:29:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,15 @@ console.log('Loaded!');
 //Change text of an Div element
 var ele=document.getElementById('lblmsg1');
 ele.innerHTML='This is new value from *.js file.';
+
+//Shift an image, when user click on that image
+var ig=document.getElementById('img11');
+var mrleft=0;
+function moveR(){
+    mrleft=mrleft+10;
+    ig.style.marginLeft=mrleft+'px';
+}
+
+ig.onclick=function(){
+    var interval=setInterval(moveR,50);
+};
